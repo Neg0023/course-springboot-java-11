48f43ed364e22a5d69f51ed2923884ca9cc16fd4
Uemerson Santos <uemerson161@hotmail.com>
2022-08-06 16:25:50 -0300

    Category

---
+++ src/main/java/com/coursejava/course/repositories/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.coursejava.course.repositories;
+
+import com.coursejava.course.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
